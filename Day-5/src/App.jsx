cf3ece29a759a0b415a72e3d6a46e4fbc5891e18
Abdul Raheem <abdulrahem20053@gmail.com>
2026-02-06 18:39:35 +0530

    INITIAL COMMIT

---
+++ Day-5/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "./Components/Button/Button";
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+    <div>
+      <Button />
+      <Header />
+    </div>
+  );
+}
+export default App;
