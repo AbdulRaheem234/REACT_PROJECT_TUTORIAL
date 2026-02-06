c9f835f0a96980a21f43159a1701d99221b1a3cd
Abdul Raheem <abdulrahem20053@gmail.com>
2026-02-06 14:23:44 +0530

    INITIAL COMMIT

---
+++ Day-3/src/App.jsx
@@ -0,0 +1,41 @@
+import Card from "./Components/Card";
+function App() {
+  return (
+    <div className="card-items">
+      <Card
+        user="Card-1"
+        img="https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29kaW5nfGVufDB8fDB8fHww"
+      />
+      <Card
+        user="Card-2"
+        img="https://media.istockphoto.com/id/1530973530/photo/software-development-concept.webp?a=1&b=1&s=612x612&w=0&k=20&c=NXxmootfVkI2C_JS5-5p06qMD_ngxJnH8BfLxnoQKP8="
+      />
+      <Card
+        user="Card-3"
+        img="https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29kaW5nfGVufDB8fDB8fHww"
+      />
+      <Card
+        user="Card-4"
+        img="https://images.unsplash.com/photo-1587620962725-abab7fe55159?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww"
+      />
+      <Card
+        user="Card-5"
+        img="https://media.istockphoto.com/id/2160850994/photo/asian-software-developers-working-on-multiple-screens-displaying-code-and-application.webp?a=1&b=1&s=612x612&w=0&k=20&c=IDY__FXpAaireo52edTqK9aEQSV9BqNaGjsZkLyf5xI="
+      />
+      <Card
+        user="Card-6"
+        img="https://media.istockphoto.com/id/2156387160/photo/hispanic-latin-american-couple-software-engineer-developer-use-computer-work-on-program.webp?a=1&b=1&s=612x612&w=0&k=20&c=MwCwZmlsa6rhG5ppvCCN-OR67mdcx2WkD5SUouSzSrE="
+      />
+      <Card
+        user="Card-7"
+        img="https://media.istockphoto.com/id/2150988877/photo/professional-man-working-on-laptop-in-modern-office.webp?a=1&b=1&s=612x612&w=0&k=20&c=LN1RZZFSzq2ceTHlM8y05p3jPcuFelJgSU_irNeBjS4="
+      />
+      <Card
+        user="Card-8"
+        img="https://media.istockphoto.com/id/616902766/photo/dedicated-to-software-development.jpg?s=612x612&w=0&k=20&c=DbeQZha7EDOxSCK49IOzP6DaVJaBElzFItOVJKQSDek="
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ Day-3/src/Components/Card.jsx
@@ -0,0 +1,12 @@
+function Card(props) {
+  return (
+    <div className="card">
+      <img src={props.img} alt="soon....." />
+      <h1>{props.user}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <button>View Profile</button>
+    </div>
+  );
+}
+
+export default Card;
